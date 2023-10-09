ce565f4a7b53c5e37ce42c80d731943d19292102
ryanmichaelcurry@gmail.com <ryanmichaelcurry@gmail.com>
2023-10-09 15:34:40 -0500

    wp_head and wp_footer

--- front-page.php
+++ front-page.php
@@ -267,10 +267,9 @@
         <div class="container"><small>Copyright &copy; CS4MS 2023</small></div>
     </div>
 
-    <!-- Bootstrap core JS-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- Core theme JS-->
-    <script src="js/scripts.js"></script>
+    <?php
+    wp_footer();
+    ?>
 </body>
 
 </html>
--- functions.php
+++ functions.php
@@ -1,9 +1,18 @@
 <?php
 
 function corporafun_register_styles() {
-    wp_enqueue_style('corporafun-bootstrap', get_template_directory_uri() . "/style.css", array(), '1.0', 'all');
+    $version = wp_get_theme()->get('Version');
+    wp_enqueue_style('corporafun-style', get_template_directory_uri() . "/style.css", array(), $version, 'all');
 }
 
 add_action('wp_enqueue_scripts', 'corporafun_register_styles');
+
+function corporafun_register_scripts() {
+    //$version = wp_get_theme()->get('Version'); 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js'
+    wp_enqueue_script('corporafun-script', get_template_directory_uri() . "/assets/js/scripts.js", array(), '1.0', true);
+    wp_enqueue_script('corporafun-bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js', array(), '5.2.3', true);
+}
+
+add_action('wp_enqueue_scripts', 'corporafun_register_scripts');
 
 ?>
